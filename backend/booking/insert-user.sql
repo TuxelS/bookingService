b80c6d4581c6f4cdcd58f75aa2048a97955be612
Anton Belov <belovanton.deal@gmail.com>
2026-02-08 22:45:55 +0300

    удаление лишнего sql запроса

--- backend/booking/insert-user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (id, email, full_name, password) VALUES
-('random_user@gmail.com', 'Жуков Анатолий Евгеньевич', '$2a$12$xPxz0Y7pqs1Bfc16UJvO6.SMmgyfYmuDABsvUefW/SPOeqXdfWDIm');
--- пароль - password
